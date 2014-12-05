8e5013a60b9d28786af07d743f2b0845835eecb6
Trevor Buckner <calculuschild@gmail.com>
2014-12-04 18:22:30 -0700

    Some testing of expanding side-bar

--- js/script.js
+++ js/script.js
@@ -12,3 +12,49 @@ cssLink .type = "text/css";
 frames['main_frame'].document.body.appendChild(cssLink);*/
 /*document.getElementById("main_frame").contentWindow.location.reload(true);*/
 }
+
+/* Sidebar */
+/*function prepareSideList() {
+	var list = document.getElementById("sideList");
+	var topicList = list.querySelectorAll(".topic");
+	for ( var i = 0 i < topics.length; i++) {
+		topics[i] = topicList[i];
+	}
+	
+		
+}
+End Sidebar*/
+
+function prepareList() {
+	alert("yay!");
+	//$('#expList')
+	var sideList = document.querySelectorAll('.expandable');
+	alert(sideList);
+	for ( var i = 0; i < sideList.length; i++)
+	{
+		alert(sideList[i].innerHTML + " " + i);
+		sideList[i].addEventListener('click', function(event) {
+			if (this == event.target) {
+				
+			}
+		{
+	}
+	lis.push("gay");
+	alert(lis);
+}
+	/*
+  .find('li:has(ul)')
+  	.click( function(event) {
+  		if (this == event.target) {
+  			$(this).toggleClass('expanded');
+  			$(this).children('ul').toggle('medium');
+  		}
+  		return false;
+  	})
+  	.addClass('collapsed')
+  	.children('ul').hide();
+  };
+ 
+  $(document).ready( function() {
+      prepareList();
+  });*/
